a8c6ee037e1646efd6d52ad3729a88177788d92b
Blaise <blaise.mahamoodally@gmail.com>
2025-12-23 20:46:53 -0500

    feat: Added Output Formatter

---
+++ cache/outputfmt.go
@@ -0,0 +1,15 @@
+package cache
+
+import (
+	"fmt"
+)
+
+func (c *Cache) Print() {
+    fmt.Println("Cache contents (head to tail):")
+    current := c.head
+    for current != nil {
+        fmt.Printf("  %s: %s\n", current.Key, current.Value)
+        current = current.Next
+    }
+    fmt.Printf("Size: %d/%d\n\n", len(c.data), c.capacity)
+}
