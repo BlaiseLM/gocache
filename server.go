c6889aa9e34c87f808666659f74aa740a794a8c0
Blaise <blaise.mahamoodally@gmail.com>
2026-01-01 00:32:38 -0500

    feat: Implemented Connection Handling

--- server.go
+++ server.go
@@ -3,25 +3,64 @@ package main
 import (
 	"fmt"
 	"net"
-	"strconv"
+	"github.com/BlaiseLM/gocache/cache"
+	"strings"
 )
 
 func main() { 
-	listener, _ := net.Listen("tcp", ":8080") // creates server and starts listening for connections
+	cache := cache.NewCache(1024)
+	listener, _ := net.Listen("tcp", ":8080") 
 	for { 
-		connection, _ := listener.Accept() // catch a connection
-		go handleConnection(connection) // allows connections to be handled concurrently (connection 1 is handled at the same time as connection 2, connection 3, etc. )
+		connection, _ := listener.Accept() 
+		go handleConnection(connection, cache) 
 	}
 }
 
-func handleConnection(connection net.Conn) { 
-	buffer := make([]byte, 4096)
-	read, _ := connection.Read(buffer)
+func handleConnection(connection net.Conn, cache *cache.Cache) { 
+	defer connection.Close()
+	for { 
+		buffer := make([]byte, 4096)
+		read, error := connection.Read(buffer)
+		if error != nil {
+			fmt.Println("ERROR: Unable to read requests", error)
+			return
+		}
+		if read == 0 {
+			return
+		}
+		request := strings.Fields(string(buffer[:read]))
+		if len(request) == 0 { 
+			continue
+		}
+		switch method := strings.ToLower(request[0]); method { 
+		case "get" :
+			if len(request) < 2 {
+				connection.Write([]byte("ERROR: GET requires a key\n"))
+				return
+			}
+			key := request[1]
+			value, ok := cache.Get(key)
+			if ok { 
+				connection.Write([]byte(value + "\n"))  
+			} else {
+				connection.Write([]byte("(nil)\n"))
+			}
+		case "set" :
+			if len(request) < 3 {
+				connection.Write([]byte("ERROR: SET requires key and value\n"))
+				return
+			}
+			key := request[1]
+			value := request[2]
+			cache.Set(key, value)
+			connection.Write([]byte("OK\n"))  
 
-	fmt.Println("Number of bytes read: " + strconv.Itoa(read))
-	fmt.Println("Content: " + string(buffer[:read]))
+		case "end" : 
+			connection.Write([]byte("Closing connection\n"))
+			return
 
-	write, _ := connection.Write(buffer[:read])
-	fmt.Println("Number of bytes written: " + strconv.Itoa(write))
-	fmt.Println("Content: " + string(buffer[:write]))
+		default: 
+			connection.Write([]byte("ERROR: Unknown command\n"))
+		}	
+	}
 }
