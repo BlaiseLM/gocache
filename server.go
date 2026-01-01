99e55bf85bfd093cba56e3e62a751f33b93cc2c5
Blaise <blaise.mahamoodally@gmail.com>
2026-01-01 11:35:37 -0500

    refactor: Added Logs and TODOs

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"github.com/BlaiseLM/gocache/cache"
 	"strings"
@@ -9,20 +9,28 @@ import (
 
 func main() { 
 	cache := cache.NewCache(1024)
-	listener, _ := net.Listen("tcp", ":8080") 
+	listener, error := net.Listen("tcp", ":8080") 
+	if error != nil { 
+		log.Fatalf("FATAL ERROR: Unable to start server [%v]\n", error)
+	}
 	for { 
-		connection, _ := listener.Accept() 
+		connection, error := listener.Accept() 
+		if error != nil { 
+			log.Printf("ERROR: Unable to accept connection [%v]\n", error)
+			// TODO: Retry accepting connection (with exp backoff?)
+		}
 		go handleConnection(connection, cache) 
 	}
 }
 
 func handleConnection(connection net.Conn, cache *cache.Cache) { 
+	// TODO: Check for broken conections
 	defer connection.Close()
 	for { 
 		buffer := make([]byte, 4096)
 		read, error := connection.Read(buffer)
 		if error != nil {
-			fmt.Println("ERROR: Unable to read requests", error)
+			log.Printf("ERROR: Unable to read requests [%v]", error)
 			return
 		}
 		if read == 0 {
@@ -32,11 +40,11 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 		if len(request) == 0 { 
 			continue
 		}
+		// TODO: Handle malformed requests (missing args, etc)
 		switch method := strings.ToLower(request[0]); method { 
 		case "get" :
 			if len(request) < 2 {
 				connection.Write([]byte("ERROR: GET requires a key\n"))
-				return
 			}
 			key := request[1]
 			value, ok := cache.Get(key)
@@ -48,7 +56,6 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 		case "set" :
 			if len(request) < 3 {
 				connection.Write([]byte("ERROR: SET requires key and value\n"))
-				return
 			}
 			key := request[1]
 			value := request[2]
