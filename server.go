5cb144166269835ac40c07732fb6cc3fb64ff55d
Blaise <blaise.mahamoodally@gmail.com>
2026-01-03 01:01:07 -0500

    refactor: Added Proper Error Handling

--- server.go
+++ server.go
@@ -8,29 +8,28 @@ import (
 )
 
 func main() { 
-	cache := cache.NewCache(1024)
-	listener, error := net.Listen("tcp", ":8080") 
-	if error != nil { 
-		log.Fatalf("FATAL ERROR: Unable to start server [%v]\n", error)
-	}
-	for { 
-		connection, error := listener.Accept() 
-		if error != nil { 
-			log.Printf("ERROR: Unable to accept connection [%v]\n", error)
-			// TODO: Retry accepting connection (with exp backoff?)
-		}
-		go handleConnection(connection, cache) 
-	}
+    cache := cache.NewCache(1024)
+    listener, err := net.Listen("tcp", ":8080") 
+    if err != nil { 
+        log.Fatalf("FATAL ERROR: Unable to start server [%v]\n", err)
+    }
+    for { 
+        connection, err := listener.Accept()
+        if err != nil {
+            log.Printf("ERROR: Failed to accept connection [%v]\n", err)
+            continue 
+        }
+        go handleConnection(connection, cache)
+    }
 }
 
 func handleConnection(connection net.Conn, cache *cache.Cache) { 
-	// TODO: Check for broken conections
 	defer connection.Close()
 	for { 
 		buffer := make([]byte, 4096)
-		read, error := connection.Read(buffer)
-		if error != nil {
-			log.Printf("ERROR: Unable to read requests [%v]", error)
+		read, err := connection.Read(buffer)
+		if err != nil {
+			log.Printf("ERROR: Unable to read requests [%v]", err)
 			return
 		}
 		if read == 0 {
@@ -40,11 +39,11 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 		if len(request) == 0 { 
 			continue
 		}
-		// TODO: Handle malformed requests (missing args, etc)
 		switch method := strings.ToLower(request[0]); method { 
 		case "get" :
 			if len(request) < 2 {
 				connection.Write([]byte("ERROR: GET requires a key\n"))
+				continue
 			}
 			key := request[1]
 			value, ok := cache.Get(key)
@@ -56,12 +55,12 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 		case "set" :
 			if len(request) < 3 {
 				connection.Write([]byte("ERROR: SET requires key and value\n"))
+				continue
 			}
 			key := request[1]
 			value := request[2]
 			cache.Set(key, value)
 			connection.Write([]byte("OK\n"))  
-
 		case "end" : 
 			connection.Write([]byte("Closing connection\n"))
 			return
