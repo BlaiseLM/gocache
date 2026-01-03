de5a565c45b4605f641eb712159af0ff1302210d
Blaise <blaise.mahamoodally@gmail.com>
2026-01-03 15:59:56 -0500

    refactor: Fixed Formatting

--- cache/outputfmt.go
+++ cache/outputfmt.go
@@ -5,11 +5,11 @@ import (
 )
 
 func (c *Cache) Print() {
-    fmt.Println("Cache contents (head to tail):")
-    current := c.head
-    for current != nil {
-        fmt.Printf("  %s: %s\n", current.Key, current.Value)
-        current = current.Next
-    }
-    fmt.Printf("Size: %d/%d\n\n", len(c.data), c.capacity)
+	fmt.Println("Cache contents (head to tail):")
+	current := c.head
+	for current != nil {
+		fmt.Printf("  %s: %s\n", current.Key, current.Value)
+		current = current.Next
+	}
+	fmt.Printf("Size: %d/%d\n\n", len(c.data), c.capacity)
 }
