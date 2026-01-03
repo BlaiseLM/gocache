9432ff4c613cdd77f2d0d308528b0475daece765
Blaise <blaise.mahamoodally@gmail.com>
2026-01-03 16:00:15 -0500

    refactor: Fixed Formatting

--- server.go
+++ server.go
@@ -1,31 +1,31 @@
 package main
 
 import (
+	"github.com/BlaiseLM/gocache/cache"
 	"log"
 	"net"
-	"github.com/BlaiseLM/gocache/cache"
 	"strings"
 )
 
-func main() { 
-    cache := cache.NewCache(1024)
-    listener, err := net.Listen("tcp", ":8080") 
-    if err != nil { 
-        log.Fatalf("FATAL ERROR: Unable to start server [%v]\n", err)
-    }
-    for { 
-        connection, err := listener.Accept()
-        if err != nil {
-            log.Printf("ERROR: Failed to accept connection [%v]\n", err)
-            continue 
-        }
-        go handleConnection(connection, cache)
-    }
+func main() {
+	cache := cache.NewCache(1024)
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("FATAL ERROR: Unable to start server [%v]\n", err)
+	}
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Printf("ERROR: Failed to accept connection [%v]\n", err)
+			continue
+		}
+		go handleConnection(connection, cache)
+	}
 }
 
-func handleConnection(connection net.Conn, cache *cache.Cache) { 
+func handleConnection(connection net.Conn, cache *cache.Cache) {
 	defer connection.Close()
-	for { 
+	for {
 		buffer := make([]byte, 4096)
 		read, err := connection.Read(buffer)
 		if err != nil {
@@ -36,23 +36,23 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 			return
 		}
 		request := strings.Fields(string(buffer[:read]))
-		if len(request) == 0 { 
+		if len(request) == 0 {
 			continue
 		}
-		switch method := strings.ToLower(request[0]); method { 
-		case "get" :
+		switch method := strings.ToLower(request[0]); method {
+		case "get":
 			if len(request) < 2 {
 				connection.Write([]byte("ERROR: GET requires a key\n"))
 				continue
 			}
 			key := request[1]
 			value, ok := cache.Get(key)
-			if ok { 
-				connection.Write([]byte(value + "\n"))  
+			if ok {
+				connection.Write([]byte(value + "\n"))
 			} else {
 				connection.Write([]byte("(nil)\n"))
 			}
-		case "set" :
+		case "set":
 			if len(request) < 3 {
 				connection.Write([]byte("ERROR: SET requires key and value\n"))
 				continue
@@ -60,8 +60,8 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 			key := request[1]
 			value := request[2]
 			cache.Set(key, value)
-			connection.Write([]byte("OK\n"))  
-		case "delete" :
+			connection.Write([]byte("OK\n"))
+		case "delete":
 			if len(request) < 2 {
 				connection.Write([]byte("ERROR: DELETE requires a key\n"))
 				continue
@@ -69,18 +69,18 @@ func handleConnection(connection net.Conn, cache *cache.Cache) {
 			key := request[1]
 			cache.Delete(key)
 			connection.Write([]byte("OK\n"))
-		case "flush" : 
-			if len(request) > 1 { 
+		case "flush":
+			if len(request) > 1 {
 				connection.Write([]byte("ERROR: FLUSH doesn't require key and/or value\n"))
 				continue
 			}
 			cache.Flush()
 			connection.Write([]byte("OK\n"))
-		case "end" : 
+		case "end":
 			connection.Write([]byte("Closing connection\n"))
 			return
-		default: 
+		default:
 			connection.Write([]byte("ERROR: Unknown command\n"))
-		}	
+		}
 	}
 }
